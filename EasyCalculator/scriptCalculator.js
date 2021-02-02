bce10d379c7fea1a4dc7ba36c5812a4d0e5812e3
Oleksandr Kravchenko <kokobillsoft@gmail.com>
2021-02-02 15:03:58 +0200

    calculator without equal

---
+++ EasyCalculator/scriptCalculator.js
@@ -0,0 +1,21 @@
+function insert(num){
+    //alert (num);
+    document.getElementById('textview').value = document.getElementById('textview').value  + num;
+    //alert (document.getElementById ('textview').value);
+}
+
+function test(){
+    alert ("good");
+}
+
+function clean() {
+   // alert("good");
+    document.getElementById('textview').value = "";
+}
+
+function back1(){
+    
+    var line = document.getElementById('textview').value;
+   // alert (line);
+    document.getElementById('textview').value = line.substring(0,line.length-1);
+}
