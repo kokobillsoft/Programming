5937786506b6eba917f3e6a9c98c50c43dac25a7
Oleksandr Kravchenko <kokobillsoft@gmail.com>
2021-02-03 22:52:36 +0200

    full ToDo List Work

--- ToDoList/script/script.js
+++ ToDoList/script/script.js
@@ -1,7 +1,12 @@
 let addMessage = document.querySelector('.message'),
 addButton = document.querySelector('.add');
-todo=document.querySelector
+todo=document.querySelector ('.todo');
 let todoList = [];
+
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages();
+}
 
 addButton.addEventListener('click', function (){
 
@@ -13,29 +18,57 @@ let newTodo = {
 
     todoList.push(newTodo);
     displayMessages();
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    addMessage.value='';
 });
 
 function displayMessages(){
     
     let displayMessage = '';
     
+    if(todoList.length ===0){
+        todo.innerHTML = '';
+    }
+
     todoList.forEach(function(item, i){
         displayMessage += `
         <li>
-            <input type='checkbox' id='item_${i}'>
-            <label for='item_${i}'>${item.todo}</label>
+            <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+            <label for='item_${i}' class="${item.important ? 'important' : ''}" >${item.todo}</label>
         </li>
         `;
-        console.log (displayMessage);
+      //  console.log (displayMessage);
         todo.innerHTML = displayMessage;
-        alert("1");
+      //  alert("1");
     });
-
 }
 
-/*function displayMessages(){
-    let displayMessage = `Our bussines: ${addMessage.value}`;
-    console.log('displayMessage% ', displayMessage);
-}*/
+todo.addEventListener('change', function(event){
+    let idInput=event.target.getAttribute('id');
+    let forLabel = todo.querySelector('[for=' + idInput +']');
+    let valueLabel = forLabel.innerHTML;
+    // change this string let valueLabel = todo.querySelector('[for=' + event.target.getAttribute('id') +']').innerHTML;
+
+    todoList.forEach(function(item){
+        if (item.todo === valueLabel){
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList))};
+    });
+});
+
+todo.addEventListener('contextmenu', function(event){
+    event.preventDefault();
+    todoList.forEach(function(item, i){
+        if(item.todo === event.target.innerHTML){
+            if(event.ctrlKey || event.metaKey){
+                todoList.splice(i, 1);
+            }else{
+                item.important = !item.important;
+            }
+            displayMessages();
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
 
 
