f30df6b8ec91b226bbf9c2424cfd5954bc746f47
Oleksandr Kravchenko <kokobillsoft@gmail.com>
2021-03-15 16:24:45 +0200

    Work Bot on RPI

--- WhiteBitBitcoinBot/script.js
+++ WhiteBitBitcoinBot/script.js
@@ -7,6 +7,51 @@ const bot = new TelegramBot(TOKEN, {polling: true});
 const MoneyMarket = "BTC_UAH";
 let actualPrice = "1";
 let previousPrice = "1";
+let lowestPrice = 10000000000;
+let higestPrice =0;
+let CHAT_ID
+
+
+bot.on("message", (msg)=>{
+
+    console.log("Bot Starting");
+    CHAT_ID = msg.chat.id;
+
+    if (msg.text == "Ноль")
+    {
+        higestPrice = actualPrice;
+        lowestPrice = actualPrice;
+        bot.sendMessage (CHAT_ID, "Ноль котировки = " + actualPrice);
+    }
+
+    if (msg.text == "Установить ноль");
+    {
+
+    }
+
+    if (msg.text == "Мин")
+    {
+        bot.sendMessage (CHAT_ID, "Минимум за сегодня = " + lowestPrice);
+    }
+
+    if (msg.text == "Макс")
+    {
+        bot.sendMessage (CHAT_ID, "Максимум за сегодня = " + higestPrice);
+    }
+    
+    if ((msg.text == "Курс") || (msg.text == "курс")){
+        bot.sendMessage (CHAT_ID, "Текущий курс = " + actualPrice);
+    }
+    console.log(msg);
+    if (msg.text == "Hi"){
+        bot.sendMessage (CHAT_ID, msg.chat.first_name + " Hi");
+    }
+    else if (msg.text == "Piska"){
+        bot.sendMessage (CHAT_ID, msg.chat.first_name + " Слыш, ты че, берега попутал?");
+    } 
+    
+});
+
 
 const request = require('request');
 
@@ -16,33 +61,30 @@ setInterval(function(){
     if (err) { return console.log(err); }
        previousPrice = actualPrice; 
        actualPrice = body.result.bid;
+       
        let changeCourse = actualPrice/previousPrice;
        console.log (changeCourse);
-       if (changeCourse>1.001)
+       if (actualPrice>higestPrice)
        {
+           higestPrice = actualPrice;
+           bot.sendMessage (CHAT_ID, "Максимум за сегодня = " + higestPrice);
+       }
+       if (actualPrice<lowestPrice)
+       {
+           lowestPrice = actualPrice;
+           bot.sendMessage (CHAT_ID, "Минимум за сегодня = " + lowestPrice);
+       }
+      // console.log (changeCourse);
+       if (changeCourse>=1.01)
+       {
+            bot.sendMessage (CHAT_ID, "Курс падает = " + actualPrice);
             console.log ("Upper");
-       } else if (changeCourse<0.998)
+       } else if (changeCourse<=0.99)
        {
+            bot.sendMessage (CHAT_ID, "Курс растет = " + actualPrice);
             console.log ("Lowwer");
        }
 
         
 });
 },xSecond);
-
-bot.on('message', (msg)=>{
-
-    console.log("Bot Starting");
-
-    if ((msg.text == "Курс") || (msg.text == "курс")){
-        bot.sendMessage (msg.chat.id, actualPrice);
-    }
-    console.log(msg);
-    if (msg.text == "Hi"){
-        bot.sendMessage (msg.chat.id, msg.chat.first_name + " Hi");
-    }
-    else if (msg.text == "Piska"){
-        bot.sendMessage (msg.chat.id, msg.chat.first_name + " Слыш, ты че, берега попутал?");
-    } 
-    
-})
